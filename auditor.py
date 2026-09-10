925b9eb0d7ff80be7985a2e27237bff6fdf67282
xiu jee <xiujee02@gmail.com>
2026-09-10 20:36:00 +0800

    requirement 8, quit function with failed entries and inventory count

--- auditor.py
+++ auditor.py
@@ -1,10 +1,15 @@
 inventory = 0
-
+failcount = 0
 while True:
     userInput = input("Enter a stock quantity to add (or type 'exit' to quit): ")
-    if not userInput.isdigit():
-        print("Invalid input. Please enter a valid stock quantity.")
-        continue
+    if userInput.lower() == 'quit':
+        print("Total Units Processed: ", inventory)
+        print("Failed entries: ", failcount)
+        break
+    elif not userInput.isdigit():
+            print("Invalid input. Please enter a valid stock quantity.")
+            failcount += 1
+            continue
     else:
         inventory += int(userInput)
         print(f"Current Inventory: {inventory}")
