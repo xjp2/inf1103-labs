9b86b8b523d0c3eb2f452753b510bbeae4b09e17
xiu jee <xiujee02@gmail.com>
2026-09-10 13:43:56 +0800

    requirement 3&4 integers only and error catching

--- auditor.py
+++ auditor.py
@@ -2,3 +2,6 @@ inventory = 0
 
 while True:
     userInput = input("Enter a stock quantity to add (or type 'exit' to quit): ")
+    if not userInput.isdigit():
+        print("Invalid input. Please enter a valid stock quantity.")
+        continue
