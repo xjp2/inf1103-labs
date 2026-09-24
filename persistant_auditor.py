5f2fe3386f5b89717281b03613d86e067d4507ac
xiu jee <xiujee02@gmail.com>
2026-09-24 13:41:13 +0800

    successfully tracking history

--- persistant_auditor.py
+++ persistant_auditor.py
@@ -1,6 +1,18 @@
 def load_inventory():
     with open("inventory.txt", "a+") as file:
+        file.seek(0)
         inventory_data = file.read()
-        print(inventory_data)
-
+        if inventory_data == "":
+            file.write("Current Orders:\n")
+        
+history = []
 load_inventory()
+while True:
+    productInput = input("Enter product name: ")
+    QuantityInput = input("Enter Quantity: ")
+    if not QuantityInput.isdigit():
+        print("Invalid input. Please enter a valid stock quantity.")
+        continue
+    else:
+        history.append((productInput, int(QuantityInput)))
+    print("Current Inventory: ", history)
